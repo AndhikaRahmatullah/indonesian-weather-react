087dca830fcd6926b302bc3f9d002f66ae7251fc
AndhikaRahmatullah <dikarahmatullah909@gmail.com>
2022-12-19 14:43:15 +0700

    add responsive navigation bar and reference city

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,26 @@
-import React from "react";
+import { useEffect } from "react";
 
 const Footer = () => {
+	useEffect(() => {
+		const footerRef = document.getElementById("footerRef");
+
+		setTimeout(() => {
+			footerRef.classList.remove("hidden");
+			footerRef.classList.add("flex");
+		}, 1000);
+	}, []);
+
 	return (
-		<div className="mt-12 mb-2 flex justify-center">
-			<p className="text-base font-normal text-dark/50">Copyright &copy; 2022 Cuaca Indonesia - All Right Reserved.</p>
+		<div
+			id="footerRef"
+			className="container mt-12 mb-2 hidden justify-center">
+			{/* phone */}
+			<p className="text-center text-sm font-normal text-dark/50 md:hidden md:text-base">
+				Copyright &copy; 2022 Cuaca Indonesia <span className="block">All Right Reserved</span>
+			</p>
+
+			{/* laptop */}
+			<p className="hidden text-base font-normal text-dark/50 md:block">Copyright &copy; 2022 Cuaca Indonesia - All Right Reserved.</p>
 		</div>
 	);
 };
